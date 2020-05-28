c3ac1d266f8dd014cc60ce4f5205a227c9d73650
Suraj Rane <surajrane42@gmail.com>
2020-05-28 20:35:17 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <new-quote @quoteAdded="addQuote"></new-quote>
+    <all-quotes :quotes="quotes" @quoteDeleted="deleteQuote"></all-quotes>
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="alert alert-info text-center">Info: Click on Quote to DELETE it!</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+export default {
+  data() {
+    return {
+      quotes: ["My first Quote"],
+      maxQuotes: 10
+    };
+  },
+  components: {
+    allQuotes: QuoteGrid,
+    newQuote: NewQuote,
+    appHeader: Header
+  },
+  methods: {
+    addQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Delete some Quotes First!");
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <app-quote
+      v-for="(quote,index) in quotes"
+      :key="index"
+      @click.native="deleteQuote(index)"
+    >{{quote}}</app-quote>
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote.vue";
+export default {
+  components: {
+    appQuote: Quote
+  },
+  props: ["quotes"],
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
